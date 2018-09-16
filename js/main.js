217dc8fee73f9efe2a1c699253c6b396ba41cbcb
mediumwb <medium4uk@gmail.com>
2018-09-17 00:37:14 +0300

    16.09 changes

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+      $('.nav__min').click(function () {
+        $('.nav ul').fadeToggle();
+    });
+   if ($( window ).width() <= 774) { //774px
+    $('#services').slick({
+        slidesToShow: 1
+        , slidesToScroll: 1
+        , arrows: false
+        , fade: true
+        , asNavFor: '#services2'
+    });
+    $('#services2').slick({
+        slidesToShow: 3
+        , slidesToScroll: 1
+        , asNavFor: '#services'
+        , dots: false
+        , centerMode: true
+        , focusOnSelect: true
+        , arrows: false
+    });
+   };
+  
+});
