786a7416d17935cb62633f0681cd14b1f84dbac0
mediumwb <medium4uk@gmail.com>
2018-09-17 15:31:20 +0300

    17.09 changes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,41 @@
 $(document).ready(function () {
-      $('.nav__min').click(function () {
+    $('.nav__min').click(function () {
         $('.nav ul').fadeToggle();
     });
-   if ($( window ).width() <= 774) { //774px
-    $('#services').slick({
-        slidesToShow: 1
-        , slidesToScroll: 1
-        , arrows: false
-        , fade: true
-        , asNavFor: '#services2'
-    });
-    $('#services2').slick({
-        slidesToShow: 3
-        , slidesToScroll: 1
-        , asNavFor: '#services'
-        , dots: false
-        , centerMode: true
-        , focusOnSelect: true
-        , arrows: false
-    });
-   };
-  
+    if ($(window).width() <= 774 && $(window).width() > 422) {
+        $('#services').slick({
+            slidesToShow: 1
+            , slidesToScroll: 1
+            , arrows: false
+            , fade: true
+            , asNavFor: '#services2'
+        });
+        $('#services2').slick({
+            slidesToShow: 3
+            , slidesToScroll: 1
+            , asNavFor: '#services'
+            , dots: false
+            , centerMode: true
+            , focusOnSelect: true
+            , arrows: false
+        });
+    }
+    if ($(window).width() <= 422) {
+        $('#services').slick({
+            slidesToShow: 1
+            , slidesToScroll: 1
+            , arrows: false
+            , fade: true
+            , asNavFor: '#services2'
+        });
+        $('#services2').slick({
+            slidesToShow: 2
+            , slidesToScroll: 1
+            , asNavFor: '#services'
+            , dots: false
+            , centerMode: true
+            , focusOnSelect: true
+            , arrows: false
+        });
+    }
 });
